affad977646866daefc97980216bf9ca2bac3c92
Tanguy <Tanguy.duwa@gmail.com>
2019-06-25 14:39:34 +0200

    affichage pokemon

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
+var Pokedex = require('pokedex-promise-v2');
+var P = new Pokedex();
+
+P.getPokemonByName(34, function(response, error) { // with callback
+    if(!error) {
+      console.log(response);
+    } else {
+      console.log(error)
+    }
+  });
+
 const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res){
-    res.render('pokemon');
+    
+    P.getPokemonsList({
+        limit: 150,
+        offset: 0
+      }, function(response) {
+          console.log(response)
+          res.render('pokemon', { pokemonList: response});
+        }
+    );
+
 })
 
 app.listen(3000, function () {
